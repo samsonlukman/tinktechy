bdc84472e859e2fd39086f8c1154c5764e6e3dff
Samson Lukman <37589048+samsonlukman@users.noreply.github.com>
2023-10-23 21:18:50 +0100

    Update urls.py

--- website/urls.py
+++ website/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="index"),
-    path('message/', views.message, name="message")
-
+    path('message/', views.message, name="message"),
+    path('python_course', views.python_course, name="python_course"),
+     path("pay/<str:tx_ref>", views.pay, name="py_pay"),
 ]
