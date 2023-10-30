9348ff531b6f09c0b0e0a7c862bf53f92cda9348
samsonlukman <lucassamson24@gmail.com>
2023-10-30 15:41:49 +0100

    ads.txt

--- mysite/urls.py
+++ mysite/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("website.urls"))
+    path('', include("website.urls")),
+    path('ads.txt', TemplateView.as_view(template_name='ads.txt', content_type='text/plain')),
 ]
